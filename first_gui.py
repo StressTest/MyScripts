1eb64a50dd00813ad67b44e03ba4ce927bae18d0
StressTest <stresscms@gmail.com>
2018-06-05 22:31:53 +0300

    Инициализация репозитория проектом с файлом pipupdate.py

--- first_gui.py
+++ first_gui.py
@@ -1,16 +1,28 @@
 import sys
-from PyQt5.QtWidgets import  QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QRadioButton, QPushButton
 from PyQt5.QtGui import QIcon
 
-class Example(QWidget)
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        btn = QRadioButton("Name", self)
+        self.resize(250, 150)
+        self.setWindowTitle("ups")
+
+        self.show()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    w = QWidget()
-    #w.resize(250, 150)
-    w.move(1200, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
+    # w = QWidget()
+    # #w.resize(250, 150)
+    # w.move(1200, 300)
+    # w.setWindowTitle('Simple')
+    # w.show()
+    wd = Example()
     sys.exit(app.exec())
